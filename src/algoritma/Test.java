8a6088471a4b664e3927a47885a0d72c69322b5c
leo krisnoto <krewengsexen@gmail.com>
2018-09-20 16:29:34 +0700

    tugas algoritma

---
+++ src/algoritma/Test.java
@@ -0,0 +1,13 @@
+
+package algoritma;
+
+public class Test {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Algoritma algo = new Algoritma();
+    }
+    
+}
